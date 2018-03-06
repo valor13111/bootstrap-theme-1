62a243b6d7e75fcf84a6cc9ce9bba1d6251ee5e9
Tyler Reed <valor13111@gmail.com>
2018-03-06 13:46:31 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('flickity', function() {
+  return gulp.src(['node_modules/flickity/dist/flickity.min.css'])
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+})
+
+gulp.task('js', function() {
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js', 'node_modules/flickity/dist/flickity.pkgd.min.js'])
+    .pipe(gulp.dest("src/js"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('server', ['sass'], function() {
+  browserSync.init({
+    server: "./src"
+  });
+
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+  gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['js', 'server', 'flickity']);
